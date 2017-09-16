1193ba53d1c95b2be40b96ca2de532e37fcb5cb3
Timothy Stott <stott.timothy@gmail.com>
2017-09-16 18:24:40 +0100

    :see_no_evil: Rename test directory

--- test/kms.test.ts
+++ test/kms.test.ts
@@ -1,10 +1,10 @@
 import { KMS } from "aws-sdk";
-import { decryptEnv } from '../src/kms';
+import { decryptEnv } from "../src/kms";
 
 describe("kms", () => {
   describe("decryptEnv", () => {
     it("throws an exception when the environment variable is blank", () => {
-      expect(decryptEnv('SECRET_KEY')).rejects.toEqual({
+      expect(decryptEnv("SECRET_KEY")).rejects.toEqual({
         error: "BlankEnvironmentVariableError",
         message: "Wont decrypt blank ENV: SECRET_KEY",
       });
--- tests/logger.test.ts
+++
@@ -1,8 +0,0 @@
-import { Logger } from "winston";
-import { logger } from '../src/logger';
-
-describe('logger', () => {
-  it('is an instance of Logger', () => {
-    expect(logger).toBeInstanceOf(Logger);
-  });
-})
